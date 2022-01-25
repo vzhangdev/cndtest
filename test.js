c9cdf7055740b161b1524272148f58b5d1a74675
vzhangdev <75464006+vzhangdev@users.noreply.github.com>
2022-01-25 11:02:51 -0600

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,18 @@
-console.log('cndtest')
+ var script = document.createElement('script');
+        script.src='https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js';
+        script.type='text/javascript';
+        document.getElementsByTagName('head')[0].appendChild(script);
+
+var timer = setInterval(() => {
+            if (typeof $ != "undefined") {
+                clearInterval(timer);
+                                         
+$(document).ready(function(){  
+    $("p").click(function(){  
+        alert("This paragraph was clicked.");  
+    });  
+});  
+            }
+        }, 100);
+                         
+
