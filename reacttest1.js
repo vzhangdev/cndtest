f49b9669dcff0f54960f3b08d120b06eee0d3340
vzhangdev <75464006+vzhangdev@users.noreply.github.com>
2022-01-25 11:40:42 -0600

    Update and rename reacttest.js to reacttest1.js

---
+++ reacttest1.js
@@ -0,0 +1,9 @@
+class HelloWorld extends React.Component {
+        render() {
+            return (<p>Hello world</p>);
+        }
+    }
+    ReactDOM.render(
+        <HelloWorld />,
+        document.getElementById('root')
+    );
